2e23e49e8fa20784c6ce30dd9f99c192ff9e7149
bad0dab <broadnicegamerr@gmail.com>
2025-10-27 10:22:29 +0100

    added protocol rules and client server communication

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -1,7 +1,42 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserire indirizzo IP:");
+        String ip = scanner.nextLine();
+        System.out.println("Inserire porta:");
+        int port = Integer.parseInt(scanner.nextLine());
+        
+        try (Socket socket = new Socket(ip, port)) {
+            System.out.println("Connesso al server...");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+            String string;
+            String killerWord = "0";
+
+            do {
+                System.out.println("Inserire una stringa:");
+                string = scanner.nextLine();
+                out.println(string); 
+                
+                String transformed = in.readLine();
+                System.out.println("Stringa trasformata: " + transformed);
+
+            } while (!string.equalsIgnoreCase(killerWord));
+
+            System.out.println("Programma terminato.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- server/src/main/java/com/example/Main.java
+++ server/src/main/java/com/example/Main.java
@@ -1,7 +1,53 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            ServerSocket myServerSocket = new ServerSocket(3000);
+            System.out.println("Server connesso...");
+            
+            while (true) {
+                Socket clientSocket = myServerSocket.accept();
+                new ClientHandler(clientSocket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ClientHandler extends Thread {
+        private Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+/*
+    1 = +
+    2 = -
+    3 = *
+    4 = \
+    0 = exit
+ */
+
+
+        @Override
+        public void run() {
+            try {
+
+                clientSocket.close();
+                System.out.println("Client disconnesso");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
